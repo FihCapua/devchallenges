9e5ffef02e64ecb9a7fa3aacb1b0102d95227fbc
FihCapua <fihcapua@gmail.com>
2020-09-21 21:16:28 -0300

    3º desafio - página de design de interiores (testes de disposição de layout e BEM)

--- 3-modern-interior/assets/js/main.js
+++ 3-modern-interior/assets/js/main.js
@@ -1,11 +1,11 @@
 const menu = document.querySelector('.menu');
-const btnMenuOpen = document.querySelector('.btnMenu_open');
-const btnMenuClose = document.querySelector('.btnMenu_close');
+const btnMenuOpen = document.querySelector('.header__hamburguer--open');
+const btnMenuClose = document.querySelector('.header__hamburguer--close');
 
 btnMenuOpen.addEventListener('click', function(){
-    menu.classList.add('menu_open');
+    menu.classList.add('menu__open');
 });
 
 btnMenuClose.addEventListener('click', function(){
-    menu.classList.remove('menu_open');
+    menu.classList.remove('menu__open');
 });
